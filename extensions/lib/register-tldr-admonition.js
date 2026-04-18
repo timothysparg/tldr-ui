22b1da903986bf1098ea724dcb8df68e1e262f02
Tim Sparg <6872586+timothysparg@users.noreply.github.com>
2026-04-18 11:58:05 +0200

    fix: preserve paragraphs in tldr admonitions

--- extensions/lib/register-tldr-admonition.js
+++ extensions/lib/register-tldr-admonition.js
@@ -3,12 +3,15 @@
 module.exports = function registerTldrAdmonition(registry) {
   registry.block('TLDR', function () {
     this.onContexts('paragraph', 'example', 'open')
+    this.parseContentAs('compound')
     this.process((parent, reader, attrs) => {
       attrs.name = 'tldr'
       if (!attrs.caption) {
         attrs.caption = 'TL;DR'
       }
-      return this.createBlock(parent, 'admonition', reader.readLines(), attrs)
+      const block = this.createBlock(parent, 'admonition', [], attrs, { content_model: 'compound' })
+      this.parseContent(block, reader.readLines())
+      return block
     })
   })
 }
